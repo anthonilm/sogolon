53ec469eb6e4af3297345c075088bfeecdbaaf30
anthonilm <tmcelrath26@gmail.com>
2025-08-03 22:22:24 -0500

    Unify full-page gradient across Hero, One-on-One, Designed For, and System Snapshot sections

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,25 +10,22 @@ import AnimatedSection from './components/AnimatedSection';
 export default function Home() {
   return (
     <main className="sogolon-home">
-      <Hero />
-      
       <div
         style={{
-          background: 'linear-gradient(to bottom, #0c1b19 0%, #1e2d29 30%, #434d45 60%, #ebe6d7 100%)'
+          background: 'linear-gradient(to bottom, #0f3832 0%, #113734 15%, #1e2d29 35%, #5c655d 65%, #ebe6d7 100%)'
         }}
       >
+        <Hero />
         <AnimatedSection>
           <OneOnOneCTA />
         </AnimatedSection>
         <AnimatedSection>
           <DesignedFor />
         </AnimatedSection>
+        <AnimatedSection>
+          <SystemSnapshot />
+        </AnimatedSection>
       </div>
-
-      <AnimatedSection>
-        <SystemSnapshot />
-      </AnimatedSection>
-
       <Footer />
     </main>
   );
